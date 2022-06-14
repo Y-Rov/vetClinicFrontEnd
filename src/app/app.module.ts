15809588c8793603b03bed85cb9ad1237a7b0a3f
froizerner <froizerner@gmail.com>
2022-06-15 00:29:23 +0300

    Added route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProceduresPageComponent } from './shared/components/procedureComponents/procedures-page/procedures-page.component';
-import {LayoutModule} from "./layout/layout.module";
-import {SharedModule} from "./shared/shared.module";
-import {SpecializationListComponent} from "./shared/components/specialization-list/specialization-list.component";
+import { LayoutModule } from "./layout/layout.module";
+import { SharedModule } from "./shared/shared.module";
+import { SpecializationListComponent } from "./shared/components/specialization-list/specialization-list.component";
+import { ExceptionPageComponent } from './shared/components/exceptionComponents/exception-page/exception-page.component';
+import { ExceptionDetailPageComponent } from './shared/components/exceptionComponents/exception-detail-page/exception-detail-page.component';
 
 const appRoutes: Routes = [
   {
@@ -21,8 +23,16 @@ const appRoutes: Routes = [
     component: ProceduresPageComponent
   },
   {
-    path:'specializations',
+    path: 'specializations',
     component: SpecializationListComponent
+  },
+  {
+    path: 'exceptions',
+    component: ExceptionPageComponent
+  },
+  {
+    path: 'exceptions/:id',
+    component: ExceptionDetailPageComponent
   }
 ];
 
