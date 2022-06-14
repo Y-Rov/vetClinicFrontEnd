70315d2b31e4ebddd06058be8716bfe9e7c2fe8d
= <--add>
2022-06-14 23:05:46 +0300

    refactored based on structure

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
+import {MatIconModule} from "@angular/material/icon";
+import {RouterModule} from "@angular/router";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [HeaderComponent,FooterComponent],
+  exports: [HeaderComponent,FooterComponent],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    RouterModule,
+    MatButtonModule,
+    MatToolbarModule
+  ]
+})
+export class LayoutModule { }
