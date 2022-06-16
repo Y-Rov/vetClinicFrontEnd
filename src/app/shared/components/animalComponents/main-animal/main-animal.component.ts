cb75dae0b13c53d3731594469f1da632b3481863
Maxim Kravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2022-06-16 11:02:45 +0300

    added main-animal

--- src/app/shared/components/animalComponents/main-animal/main-animal.component.ts
+++ src/app/shared/components/animalComponents/main-animal/main-animal.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {AnimalService} from "../../../../core/services/animalService/animal.service";
 import {Animal} from "../../../../core/models/Animal";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatDialog} from "@angular/material/dialog";
+import {DeleteAnimalComponent} from "../delete-animal/delete-animal.component";
+import {EditAnimalComponent} from "../edit-animal/edit-animal.component";
+import {AddAnimalComponent} from "../add-animal/add-animal.component";
 
 @Component({
   selector: 'app-main-animal',
@@ -9,21 +15,56 @@ import {Animal} from "../../../../core/models/Animal";
 })
 export class MainAnimalComponent implements OnInit {
 
-  animals : Animal[] | null = null;
+  dataSource: MatTableDataSource<Animal> = new MatTableDataSource();
+  displayedColumns: string[] = ['nickname','birthdate', 'delete','edit'];
 
-  constructor(private animalService : AnimalService)
+  @ViewChild(MatPaginator) pagination?: MatPaginator;
+
+  constructor(
+    private animalService : AnimalService,
+    private matDialog: MatDialog)
   {
     this.updateList();
   }
 
   private updateList(){
     this.animalService.getAnimals().subscribe((animals)=>{
-      this.animals = animals;
-      console.log(animals);
-    })
+      this.dataSource.data = animals;
+    });
   }
 
   ngOnInit(): void {
+    this.updateList();
+  }
+
+  ngAfterViewInit(){
+    this.dataSource.paginator = this.pagination!;
+  }
+
+  onDeleteAnimal(animal:Animal){
+    const dialogRef = this.matDialog.open(DeleteAnimalComponent,{
+      autoFocus:false,
+      data:{
+        id: animal.id
+      }
+    });
+
+    dialogRef.afterClosed().subscribe((reuireReload:boolean)=>{if(reuireReload) this.updateList()});
+  }
+
+  onEditAnimal(animal:Animal){
+    const dialogRef = this.matDialog.open(EditAnimalComponent,{
+      autoFocus:false,
+      data:animal
+    });
+
+    dialogRef.afterClosed().subscribe((reuireReload:boolean)=>{if(reuireReload) this.updateList()});
+  }
+
+  onNewAnimal(){
+    const dialogRef = this.matDialog.open(AddAnimalComponent);
+
+    dialogRef.afterClosed().subscribe((reuireReload:boolean)=>{if(reuireReload) this.updateList()});
   }
 
 }
