89ec5841c3b0952deec205d047bc24e21c55db92
YuraRov <yura.rov555@gmail.com>
2022-06-18 22:51:53 +0300

    Deleted commented code

--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -3,12 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: [
-    '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css',
-    './home.component.sass'
-  ],
+  styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
+
+  images: string[] = ['first', 'second', 'third'];
 
   constructor() { }
 
