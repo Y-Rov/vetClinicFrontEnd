f27eed09ad89bf3b8f8a9ca5f65d1febc7c3c663
sefirus <sashasam348@gmail.com>
2022-06-18 19:16:50 +0300

    Removed redundant comment

--- src/app/shared/components/procedureComponents/procedures-page/procedures-page.component.ts
+++ src/app/shared/components/procedureComponents/procedures-page/procedures-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Procedure } from '../../../../core/models/Procedure';
-import { Specialization } from '../../../../core/models/Specialization';
 import { ProcedureService } from '../../../../core/services/procedureService/procedure.service'; 
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteProcedureDialogComponent } from '../delete-procedure-dialog/delete-procedure-dialog.component';
