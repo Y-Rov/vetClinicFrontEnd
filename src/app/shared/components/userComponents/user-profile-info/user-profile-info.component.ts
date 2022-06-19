07edbeeaf150db5b263d20086eade4bac58240c5
Vitalik Knysh <knish.vital@gmail.com>
2022-06-19 10:59:44 +0300

    Small code corrections

--- src/app/shared/components/userComponents/user-profile-info/user-profile-info.component.ts
+++ src/app/shared/components/userComponents/user-profile-info/user-profile-info.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/core/models/User';
 import { UserService } from 'src/app/core/services/userService/user.service';
-import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -12,20 +11,18 @@ import { ActivatedRoute } from '@angular/router';
 export class UserProfileInfoComponent implements OnInit {
   id: number = 0;
   user: User | null = null;
-  subscription: Subscription | null = null;
 
   constructor(
     private userService: UserService, 
     private activatedRoute: ActivatedRoute) { 
-      this.subscription = this.activatedRoute.params.subscribe(params => this.id = +params['id']);
+      this.id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
     }
 
   ngOnInit(): void {
-    this.get(this.id);
+    this.getUser(this.id);
   }
 
-  get(id: number): void {
-    this.userService.getById(id)
-      .subscribe(user => this.user = user);
+  getUser(id: number): void {
+    this.userService.getById(id).subscribe(user => this.user = user);
   }
 }
