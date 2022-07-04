2f0fe48e7970393717f386029e987d6dd13acee6
Anastasia Stetsenko <mubagu905@gmail.com>
2022-07-04 14:02:23 +0300

    autorization for module

--- src/app/features/financialStatements/financial-statement-routing.module.ts
+++ src/app/features/financialStatements/financial-statement-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RolesGuard } from '../../helpers/roles-guard/roles.guard';
 import { FinancialStatementPageComponent } from './Components/financial-statement-page/financial-statement-page.component';
 
 
-const routes: Routes = [{ path: '', component: FinancialStatementPageComponent}];
+const routes: Routes = [{
+  path: '',
+  component: FinancialStatementPageComponent,
+  canActivate: [RolesGuard],
+  data: { allowedRoles: ['Accountant'] }
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
