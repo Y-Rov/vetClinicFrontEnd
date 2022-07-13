d5c4a82d92295c6ffd7dc057ffa31c79849f36f1
Anastasia Stetsenko <mubagu905@gmail.com>
2022-07-13 18:34:09 +0300

    added paginator to financial statement

--- src/app/core/services/financialService/financial-statement.service.ts
+++ src/app/core/services/financialService/financial-statement.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 import { catchError, map, Observable, of } from 'rxjs';
 import { FinStatementOneMonth } from "../../models/FinancialStatement/FinStatementOneMonth";
 import { MyDate } from '../../models/FinancialStatement/MyDate';
+import { FinancialStatementParameters } from "../../models/operational-models/QueryParameters/FinancialStatementParameters";
 import { ResourceService } from '../resourceService/resource.service';
 
 @Injectable({
@@ -18,6 +19,21 @@ export class FinancialStatementService extends ResourceService <FinStatementOneM
     super(httpClient, currentLocation, FinStatementOneMonth, 'https://localhost:5001/api/financialStatements');
   }
 
+
+  getAllFinStat( data: MyDate ,pageNumber: number = 1, pageSize: number = 4)
+    : Observable<FinancialStatementParameters> {
+    const viewModel =
+    {
+      startDate: data.startDate,
+      endDate: data.endDate
+    };
+    let url = `${this.apiUrl}?PageNumber=${pageNumber}&PageSize=${pageSize}`;
+
+    return this.http.post<FinancialStatementParameters>(url, viewModel, this.httpOptions)
+      .pipe(
+        catchError(this.handleError<FinancialStatementParameters>('GetAllFinStat'))
+      );
+  }
 
   getFinancialStatement(data: MyDate): Observable<FinStatementOneMonth[]> {
     const viewModel =
