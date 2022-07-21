ff3aaccfc288e8cd1fde8fdcd8e097adf53a0e8a
= <--add>
2022-07-21 15:18:27 +0300

    changed page options

--- src/app/features/feedback/feedbacks/feedbacks.component.ts
+++ src/app/features/feedback/feedbacks/feedbacks.component.ts
@@ -19,7 +19,7 @@ export class FeedbacksComponent implements OnInit {
   ];
 
   pageSizeOptions: { name: string; value: number }[] = [
-    { name: '4', value: 4 },
+    { name: '5', value: 5 },
     { name: '10', value: 10 },
     { name: '20', value: 20 }
   ];
