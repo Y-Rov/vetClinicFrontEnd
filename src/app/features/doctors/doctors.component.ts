8992b2b261fc0735b9d079be3f76217e8e0a85cf
Denys Liienko <lienko.d.k@gmail.com>
2022-07-26 16:09:02 +0300

    Added "ask a question" link

--- src/app/features/doctors/doctors.component.ts
+++ src/app/features/doctors/doctors.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Chat } from 'src/app/core/models/Chat';
 import { User } from "../../core/models/User";
+import { MessagingService } from '../messaging/services/messaging.service';
 import { UserService } from "../userDashboard/services/userService/user.service";
 
 @Component({
@@ -11,7 +14,9 @@ export class DoctorsComponent implements OnInit {
   doctors: User[] = [];
 
   constructor(
-    private userService: UserService) { }
+    private userService: UserService,
+    private messagingService: MessagingService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.getDoctors();
@@ -28,5 +33,16 @@ export class DoctorsComponent implements OnInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value.toLowerCase();
     this.getDoctors(filterValue);
+  }
+
+  onAskAQuestion(doctor: User) {
+    const chat: Chat = new Chat({
+      name: `${doctor.firstName} ${doctor.lastName}`,
+      interlocutorId: doctor.id,
+      picture: doctor.profilePicture
+    })
+    this.messagingService.selectedChat = chat;
+
+    this.router.navigate(['/messages']);
   }
 }
