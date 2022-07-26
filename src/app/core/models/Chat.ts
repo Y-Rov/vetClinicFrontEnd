bdf8bc19231beef292d4620db239cdc05941d248
Denys Liienko <lienko.d.k@gmail.com>
2022-07-26 16:02:42 +0300

    Updated Chat model

--- src/app/core/models/Chat.ts
+++ src/app/core/models/Chat.ts
@@ -1,11 +1,15 @@
 import { Message } from "./Message";
+import { ResourceModel } from "./ResourceModel";
 
-export class Chat {
-    id?: number;
+export class Chat extends ResourceModel<Chat>{
     name?: string;
     interlocutorId?: number;
-    picture?: string;
+    picture?: string | null| undefined;
     numberOfUnreadMessages: number = 0;
     lastMessage?: Message;
     messages?: Message[];
+
+    constructor(model?: Partial<Chat>) {
+        super(model);
+    }
 }
